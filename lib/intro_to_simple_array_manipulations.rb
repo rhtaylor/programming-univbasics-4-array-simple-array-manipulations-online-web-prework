3f3acca708b64de9f03802590c9d211961905257
Randall Taylor <randyisbusy@gmail.com>
2019-06-23 19:52:39 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,22 @@
+def using_push(array, string)
+  return array.push(string)
+end
+def using_unshift(array, string)
+  return array.unshift(string)
+end
+def using_pop(array)
+  return array.pop
+end
+def pop_with_args(array)
+return array.pop(2)
+end
+def using_shift(array)
+  return array.shift
+end
+def shift_with_args(array)
+  return array.shift(2)
+end
+def using_concat(array1, array2)
+  return array1.concat(array2)
+
+end
